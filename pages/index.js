85f5cced0db1bdab1a8b50375ad32f0ae32ea925
Gustavo Luis da Silva <desenvolvimento@criciuma.sc.gov.br>
2025-11-06 12:34:16 -0300

    iogjiweg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
+
 function Home(){
   return <h1>Calaboca Xexeto</h1>
+  
+}
+function Header(){
+  <Image src="../images/download.jfif"/>
 }
 
 export default Home;
